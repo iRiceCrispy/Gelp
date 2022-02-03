3253dea841442153e1f6eed47302be66cbc9ec6f
Eric Huang <huangeric.j@gmail.com>
2022-02-03 10:51:01 -0800

    User auth setup

---
+++ backend/db/seeders/20220203175148-demo-user.js
@@ -0,0 +1,23 @@
+'use strict';
+const bcrypt = require('bcryptjs');
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert(
+      'Users',
+      [
+        {
+          email: 'demo@user.io',
+          username: 'Demo-lition',
+          hashedPassword: bcrypt.hashSync('password'),
+        },
+      ],
+      {}
+    );
+  },
+
+  down: (queryInterface, Sequelize) => {
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete('Users', null, {});
+  },
+};
