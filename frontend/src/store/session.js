1033474a03f68998bfd74bf14a2404e038220ba1
Eric Huang <huangeric.j@gmail.com>
2022-02-04 10:52:33 -0800

    Signup form

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -17,8 +17,21 @@ export const login = user => async dispatch => {
 
   const res = await csrfFetch('/api/session', {
     method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ credential, password }),
+  });
+
+  const data = await res.json();
+  dispatch(loginUser(data.user));
+
+  return res;
+};
+
+export const signup = user => async dispatch => {
+  const { username, email, password } = user;
+
+  const res = await csrfFetch('/api/users', {
+    method: 'POST',
+    body: JSON.stringify({ username, email, password }),
   });
 
   const data = await res.json();
