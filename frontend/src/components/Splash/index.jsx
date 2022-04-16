8ae6251e0344d0db73af68679a369d7d0e98cac4
Eric Huang <huangeric.j@gmail.com>
2022-04-16 02:56:55 -0700

    Splash header restyled

--- frontend/src/components/Splash/index.jsx
+++ frontend/src/components/Splash/index.jsx
@@ -1,25 +1,51 @@
 import React from 'react';
-import Navigation from '../Navigation';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import SearchBar from '../Navigation/SearchBar';
+import ProfileButton from '../Navigation/ProfileButton';
 import GameList from './GameList';
 import './Splash.css';
 
-const Splash = () => (
-  <div className='mainContainer'>
-    <div
-      className='headContainer'
-      style={{
-        backgroundImage: 'url(\'https://thenerdstash.com/wp-content/uploads/2015/10/Project-Zomboid.png\')',
-      }}
-    >
-      <div className='innerHeadContainer'>
-        <Navigation />
+const Splash = () => {
+  const sessionUser = useSelector(state => state.session.user);
+
+  return (
+    <div className='mainContainer'>
+      <div
+        className='headContainer'
+        style={{
+          backgroundImage: 'url(\'https://thenerdstash.com/wp-content/uploads/2015/10/Project-Zomboid.png\')',
+        }}
+      >
+        <div className='innerHeadContainer'>
+          <div className='userAuth'>
+            {sessionUser ? (
+              <ProfileButton />
+            ) : (
+              <>
+                <Link className='navText login' to='/login'>
+                  Log In
+                </Link>
+                <Link className='navText logout btn btnRed' to='/signup'>
+                  Sign Up
+                </Link>
+              </>
+            )}
+          </div>
+          <a className='home' href='/'>
+            <span>Gelp</span>
+            <img src='/favicon.ico' alt='' />
+          </a>
+          <div className='searchbar'>
+            <SearchBar />
+          </div>
+        </div>
+      </div>
+      <div className='bodyContainer'>
+        <p>Find the Best Games Online</p>
+        <GameList />
       </div>
     </div>
-    <div className='bodyContainer'>
-      <p>Find the Best Games Online</p>
-      <GameList />
-    </div>
-  </div>
-);
-
+  );
+};
 export default Splash;
